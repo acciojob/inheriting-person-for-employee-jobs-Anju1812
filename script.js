4fe5bdbe68c8faf49331c05ebbd2c49a113b3059
Anju1812 <117847428+Anju1812@users.noreply.github.com>
2023-07-14 19:19:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+	this.name=name;
+	this.age=age;
+}
+Person.prototype=greet(){
+	Console.log(Hello, my name is <name>, I am <age> years old.)
+}
 
-function Employee(name, age, jobTitle) {}
+function Employee(name, age, jobTitle) {
+	Employee.prototype=Person.prototype;
+	Employee.prototype=jobGreet(){
+		console.log(Hello, my name is <name>, I am <age> years old, and my job title is <jobTitle>);
+	}
+}
 
 // Do not change code below this line
 window.Person = Person;
