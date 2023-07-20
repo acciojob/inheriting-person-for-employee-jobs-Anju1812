651af4cff4ae059815221d4cd790d6ad83e22d76
Anju1812 <117847428+Anju1812@users.noreply.github.com>
2023-07-20 20:09:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,78 @@
-// complete this js code
+Hlo, Summarize Request
+
+
+User
+Hlo
+
+ChatGPT
+Hello! How can I assist you today?
+
+
+
+
+
+User
+Where I can see my previous  chatting with you
+
+ChatGPT
+As an AI language model, I don't have access to your previous chat history. However, if you're using the same device and browser you used before, you might be able to see your previous chat with me in your chat history or browsing history.
+
+If you're using a chat service or platform that logs conversations, you might also be able to find your previous chat with me there. Some platforms allow you to review past conversations or provide an option to save chat logs.
+
+If you're using a specific application or website to interact with me, you can check if there's a history or chat log feature available to access your previous conversations. If not, unfortunately, my design doesn't include the ability to store or retrieve past interactions with users. Each conversation is treated as independent and doesn't carry over any context from previous sessions.
+
+
+
+
+
+User
+Create a constructor function called Person that takes two arguments: name and age.
+Add a method to the Person prototype called greet() that logs a greeting with the person's name and age: Hello, my name is <name>, I am <age> years old.
+Create a constructor function called Employee that takes three arguments: name, age, and jobTitle.
+Inherit the Person prototype in the Employee prototype
+Add a method to the Employee prototype called jobGreet() that logs a greeting with the person's job title: Hello, my name is <name>, I am <age> years old, and my job title is <jobTitle>.
+
+ChatGPT
+Sure, I can help you create the constructor functions and set up the inheritance. Here's the code for the Person and Employee constructor functions with the required methods:
+
+javascript
+Copy code
+// Constructor function for Person
 function Person(name, age) {
-	this.name=name;
-	this.age=age;
-}
-Person.prototype=greet(){
-	Console.log(Hello, my name is <name>, I am <age> years old.)
+  this.name = name;
+  this.age = age;
 }
 
-function Employee(name, age, jobTitle) {
-	Employee.prototype=Person.prototype;
-	this.jobTitle=jobTitle;
-	Employee.prototype=jobGreet(){
-		console.log(Hello, my name is <name>, I am <age> years old, and my job title is <jobTitle>);
-	}
+// Adding the greet method to the Person prototype
+Person.prototype.greet = function() {
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
 }
+
+// Constructor function for Employee
+function Employee(name, age, jobTitle) {
+  // Call the Person constructor to set name and age
+  Person.call(this, name, age);
+  this.jobTitle = jobTitle;
+}
+
+// Inherit the Person prototype methods into Employee prototype
+Employee.prototype = Object.create(Person.prototype);
+
+// Set the constructor property of Employee back to itself
+Employee.prototype.constructor = Employee;
+
+// Adding the jobGreet method to the Employee prototype
+Employee.prototype.jobGreet = function() {
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+}
+
+// Example usage:
+const person1 = new Person('John', 30);
+person1.greet(); // Output: Hello, my name is John, I am 30 years old.
+
+const employee1 = new Employee('Alice', 25, 'Software Engineer');
+employee1.greet(); // Output: Hello, my name is Alice, I am 25 years old.
+employee1.jobGreet(); // Output: Hello, my name is Alice, I am 25 years old, and my job title is Software Engineer.
 
 // Do not change code below this line
 window.Person = Person;
